5d73b3a873eec967a2bcd1cd8dce161a0fe831ee
Brian Roberts <brian.roberts@timgroup.com>
2015-01-20 16:16:45 -0500

    Brian: Improved BUILD_NUMBER retrieval (from sys or env).

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object SbtJavaVersionBuild extends Build with BuildExtra {
       sbtPlugin    := true,
       organization := "com.timgroup",
       name         := "sbt-java-version",
-      version      := "0.0." + System.getProperty("BUILD_NUMBER", "0-SNAPSHOT"),
+      version      := "0.0." + System.getProperty("BUILD_NUMBER", sys.env.getOrElse("BUILD_NUMBER", "0-SNAPSHOT")),
       sbtVersion   in Global := "0.13.7",
       scalaVersion in Global := "2.10.4"
     )
